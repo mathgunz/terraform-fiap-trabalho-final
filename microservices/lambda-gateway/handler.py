5829fd6c52851124930ae992aac6771692181a8d
mathgunz@gmail.com <mathgunz@gmail.com>
2020-12-11 01:16:57 -0300

    sda

--- microservices/lambda-gateway/handler.py
+++ microservices/lambda-gateway/handler.py
@@ -1,8 +1,16 @@
 import json
-
+import boto3
 
 def handler(event, context):
     usuario = event['pathParameters']['usuario'];
+    
+    sqs = boto3.resource('sqs')
+    
+    queue = sqs.get_queue_by_name(QueueName='fiap-lab-trabalho-final-dev')
+    response = queue.send_message(MessageBody=usuario)
+    
+    print(response.get('MessageId'))
+    print(response.get('MD5OfMessageBody'))
     
     body = {
         "message": "O usuario enviado foi: " + str(usuario)
