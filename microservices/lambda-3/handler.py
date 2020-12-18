58ca7f8afdc118da3e928cf6f6821d1bd5423ba2
mathgunz@gmail.com <mathgunz@gmail.com>
2020-12-18 00:39:00 -0300

    teste

--- microservices/lambda-3/handler.py
+++ microservices/lambda-3/handler.py
@@ -2,34 +2,17 @@ from sqsHandler import SqsHandler
 from snsHandler import SnsHandler
 
 def handler(event, context):
-    sqs = SqsHandler('https://sqs.us-east-1.amazonaws.com/516773109411/fiap-lab-trabalho-final-principal-dev-DLQ')
     
-    for i in range(100):
-        msgs = sqs.getMessage(10)
+    sqs = SqsHandler('https://sqs.us-east-1.amazonaws.com/516773109411/fiap-lab-trabalho-final-principal-dev-DLQ');
         
-        print('entrouuuu')
-        print(str(msgs))
+    print('Recebendo DLQ')
+    print(event['Records'][0]['body'])
         
-        sns = SnsHandler('arn:aws:sns:us-east-1:516773109411:lab-fiap-trabalho-final-topico-dev')
-            
-        response = sns.publish('será que notifica ?')
-            
-        print(response)
-        
-        # if('Messages' not in msgs):
-        #     break
-        # if(len(msgs['Messages']) == 0):
-        #     break
-        
-        # for msg in msgs['Messages']:
-        #     print('Entrou no for')
-        #     print(str(msg))
-            
-        #     sns = SnsHandler('arn:aws:sns:us-east-1:516773109411:lab-fiap-trabalho-final-topico-dev')
-            
-        #     response = sns.publish(sns)
-            
-        #     print(response)
-            
-        #     sqs.deleteMessage(msg['ReceiptHandle'])
+    sns = SnsHandler('arn:aws:sns:us-east-1:516773109411:lab-fiap-trabalho-final-topico-dev');
+    response = sns.publish(event['Records'][0]['body']);
+    print(response)
     
+    print('Email enviado.')
+    
+    sqs.deleteMessage(event['Records'][0]['receiptHandle'])
+    print('Mensagem Removida.')
