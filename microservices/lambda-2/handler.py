5ea22aac86e41d36dd3440d24811d48b3e793396
mathgunz@gmail.com <mathgunz@gmail.com>
2020-12-18 10:43:57 -0300

    ajuste

--- microservices/lambda-2/handler.py
+++ microservices/lambda-2/handler.py
@@ -5,3 +5,7 @@ def handler(event, context):
     print('Recebendo Mensagem.');
 
     print(event['Records'][0]['body']);
+    
+    print('Mensagem recebida');
+    
+    raise Exception('Devolvendo pra fila')
