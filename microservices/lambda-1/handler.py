f334cd554d5c7251f35a11ad6035acd420b0584d
mathgunz@gmail.com <mathgunz@gmail.com>
2020-12-17 21:53:35 -0300

    ajustes

--- microservices/lambda-1/handler.py
+++ microservices/lambda-1/handler.py
@@ -1,21 +1,42 @@
 import json
 import boto3
 
-def handler(event, context):
+import sys
+sys.path.insert(0, '/opt')
+
+def buscarUsuario(event, context):
     usuario = event['pathParameters']['usuario'];
-    
-    sqs = boto3.resource('sqs')
-    
-    queue = sqs.get_queue_by_name(QueueName='fiap-lab-trabalho-final-principal-dev')
-    response = queue.send_message(MessageBody=usuario)
-    
-    print(response.get('MessageId'))
-    print(response.get('MD5OfMessageBody'))
     
     body = {
         "message": "O usuario enviado foi: " + str(usuario)
     }
 
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+    
+def criarUsuario(event, context):
+    
+    print('body: ' + str(event['body']))
+    
+    usuario = event['body'];
+
+    print('Usuario: ' + str(usuario))
+    
+    sqs = boto3.resource('sqs')
+    
+    queue = sqs.get_queue_by_name(QueueName='fiap-lab-trabalho-final-principal-dev')
+    queue.send_message(MessageBody=usuario)
+    
+    print('mensagem enviada')
+    
+    body = {
+        "message": "O usuario enviado foi cadastrado"
+    }
+    
     response = {
         "statusCode": 200,
         "body": json.dumps(body)
--- microservices/lambda-2/handler.py
+++ microservices/lambda-2/handler.py
@@ -1,19 +1,43 @@
 from sqsHandler import SqsHandler
 
 def handler(event, context):
-    sqs = SqsHandler('https://sqs.us-east-1.amazonaws.com/516773109411/fiap-lab-trabalho-final-principal-dev')
     
-    for i in range(100):
+    sqs = SqsHandler('https://sqs.us-east-1.amazonaws.com/516773109411/fiap-lab-trabalho-final-principal-dev')
+
+    print('Iniciando.')
+    
+    for i in range(20):
         msgs = sqs.getMessage(10)
         
-        print('entrouuuu')
-        print(str(msgs))
+        print('Mensagens: ' + str(msgs))
         
         if('Messages' not in msgs):
+            print('Não tem mensagem.')
             break
         if(len(msgs['Messages']) == 0):
+            print('Vazio.')
             break
         
         for msg in msgs['Messages']:
-            print('Entrou no for')
-            print(str(msg))
+            print('Mensagem: ' + str(msg['Body']))
+            
+            # sqs.deleteMessage(msg['ReceiptHandle'])
+            # print('Mensagem: ' + str(msg))
+
+# def handler(event, context):
+#     sqs = SqsHandler('https://sqs.us-east-1.amazonaws.com/516773109411/fiap-lab-trabalho-final-principal-dev')
+    
+#     for i in range(100):
+#         msgs = sqs.getMessage(10)
+        
+#         print('entrouuuu')
+#         print(str(msgs))
+        
+#         if('Messages' not in msgs):
+#             break
+#         if(len(msgs['Messages']) == 0):
+#             break
+        
+#         for msg in msgs['Messages']:
+#             print('Entrou no for')
+#             print(str(msg))
